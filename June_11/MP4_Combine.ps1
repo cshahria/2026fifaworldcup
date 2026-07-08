b1bc64141f0ce9315fa590ca7ce08a526c595739
cshahria <39628218+cshahria@users.noreply.github.com>
2026-07-07 18:53:42 -0700

    20260707_185333
    Final Shift

---
+++ June_11/MP4_Combine.ps1
@@ -0,0 +1,22 @@
+﻿# 1. Change the current working directory to your local folder
+cd "C:\Users\cyrus\OneDrive\99. Archive\Old\Documents\2026\FIFA\June_Journal\2026fifaworldcup\July_6"
+
+# 2. Create the list array
+$files = @(
+	"file '2026_070739_1.mp4'"
+	"file '2026_070739_2.mp4'"
+	"file '2026_070739_3.mp4'"
+	"file '2026_070739_4.mp4'"
+	"file '2026_070739_5.mp4'"
+
+		
+)
+
+# 3. Write out using New-Item to ensure it is raw UTF-8 without hidden BOM characters
+[System.IO.File]::WriteAllLines((Get-Item .).FullName + "\inputs.txt", $files)
+
+# 4. Run FFmpeg to append the files together seamlessly
+ffmpeg -f concat -safe 0 -i inputs.txt -c copy clip.mp4
+
+# 5. Clean up the temporary text file
+Remove-Item "inputs.txt"
